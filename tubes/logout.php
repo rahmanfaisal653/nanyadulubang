8c849e393cd4ea8b497cd4ed83f859c221d597d0
rahmanfaisal653 <rahmanfaisal653@gmail.com>
2025-05-02 08:38:34 +0700

    Delete tubes directory

--- tubes/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();         // Mulai sesi
-session_unset();         // Hapus semua variabel sesi
-session_destroy();       // Hancurkan sesi
-
-header("Location: konten/logindanregister.php"); // Arahkan ke halaman login
-exit;
-?>
